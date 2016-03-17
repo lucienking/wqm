f720558a3759424d663c7cc378af26e9bc01b282
TangWeilong <371278319@qq.com>
2016-03-17 18:35:17 +0800

    增加本地地址引用

--- src/main/webapp/static/js/map/waterMap.js
+++ src/main/webapp/static/js/map/waterMap.js
@@ -2,6 +2,7 @@
  * 2016.03.04 author by tangweilong
  */
 var wkid = 4490;
+//var serviceUrl = "http://localhost:6080/arcgis/rest/services/";
 var serviceUrl = "http://10.215.201.151:6080/arcgis/rest/services/";
 var monitor_points_Url = serviceUrl + "monitor_points/MapServer";
 var water_pollution_Url = serviceUrl + "water_pollution/MapServer";
